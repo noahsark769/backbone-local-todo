92688e8aa7e8a1b206af7ec2d4e13737ff0d2066
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-06-19 01:47:14 -0700

    actually works and added hipster colors

--- js/functionality.js
+++ js/functionality.js
@@ -1,7 +1,14 @@
 var todo = todo || {};
 
 (function () {
-    var store = new Backbone.LocalStorage("items");
+    var _renderAsync = function(templateName, data, success) {
+        $.get("partials/" + templateName + ".tmpl", function (templateString) {
+            var rendered = _.template(templateString, data);
+            success(rendered);
+        });
+    };
+
+    var _store = new Backbone.LocalStorage("items");
     /**
      * We have only one model, an Item which represents a todo list item.
      */
@@ -9,20 +16,27 @@ var todo = todo || {};
         defaults: {
          name: ""
         },
-        localStorage: store
+        localStorage: _store
     });
 
-    /**
-     * Every item has a View, which keeps one item (a Model) internally and
-     * knows how to render it, here with an "li" tag.
-     */
     var ItemView = Backbone.View.extend({
         tagName: "li",
         className: "item",
+        events: {
+            "click .js-destroy": "destroy"
+        },
+        initialize: function () {
+            console.log("initializing an itemview: " + this.model.toJSON());
+            this.listenTo(this.model, "destroy", this.remove);
+        },
         render: function () {
-            console.log("Rendering a model with name: " + this.model.get("name") + "!");
-            this.$el.html(this.model.get("name"));
+            console.log("rendering an item view: " + this.model.toJSON());
+            this.$el.html("<span class='item-name'>" + this.model.get("name") + "</span><span class='destroy js-destroy'>done</span>")
             return this.$el;
+        },
+        destroy: function() {
+            console.log("bulldozin");
+            this.model.destroy();
         }
     });
 
@@ -32,39 +46,65 @@ var todo = todo || {};
      * models using a GET request to some server, but here we use local storage
      * instead.
      */
-    var Items = Backbone.Collection.extend({
+    var ItemCollection = Backbone.Collection.extend({
         model: Item,
-        localStorage: store
+        localStorage: _store
+    });
+    var Items = new ItemCollection();
+
+    var AddItemView = Backbone.View.extend({
+        el: ".add",
+        events: {
+            "click .js-add": "addItem",
+            "keypress .js-name": "addItemIfEnter"
+        },
+        initialize: function () {
+            this.$input = this.$(".js-name");
+        },
+        addItem: function () {
+            console.log("addItem clicked in add item view");
+            var name = this.$input.val();
+            Items.create({name: name});
+            this.$input.val("");
+        },
+        addItemIfEnter: function (e) {
+            if (e.keyCode == 13) {
+                this.addItem();
+                this.$input.focus();
+            }
+        }
     });
 
     var ItemListView = Backbone.View.extend({
         el: ".todo-items",
         events: {
-            "click .js-add": "addItem"
+            "click .js-add": "addItem",
+            "keypress .js-name": "addIfEnter"
+        },
+        initialize: function () {
+            Items.fetch();
+            Items.on("add", this.render, this);
+            Items.on("remove", this.render, this);
         },
         render: function () {
             var _this = this;
-            var items = new Items();
-            this.$(".item").remove();
-            items.fetch({
-                success: function (data) {
-                    data.models.forEach(function (item) {
-                        _this.$el.append(new ItemView({model: item}).render());
-                    });
-                }
+            this.$el.empty();
+            Items.forEach(function(model) {
+                var view = new ItemView({model: model});
+                _this.$el.append(view.render());
             });
-        },
-        addItem: function () {
-            console.log("calling additem");
-            var name = this.$(".js-name").val();
-            var item = new Item({name: name});
-            item.save();
-            this.render();
         }
+        // addIfEnter: function (e) {
+        //     if (e.keyCode == 13) {
+        //         this.addItem();
+        //         this.$input.focus();
+        //     }
+        // }
     });
 
     todo.start = function () {
         var itemListView = new ItemListView();
+        var addItemView = new AddItemView();
         itemListView.render();
     };
 })();
